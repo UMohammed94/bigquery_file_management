ab6c5fd195df0ad7a1bde5f57faecc0b29fc21f4
Mohammed Uddin <uddin.mohammed94@gmail.com>
2025-01-07 16:05:42 -0600

    added some checks in the data post cleanup

--- main/main.py
+++ main/main.py
@@ -1,24 +1,24 @@
-# import os
-# import pandas as pd
-# from helpers import files_to_upload_dir, csvs_to_upload_dir, dataset_id, table_id
-# from process_excel import process_excel_files
-# from upload_data_to_bigquery import get_bigquery_client, get_schema, load_csv_to_bigquery
+import os
+import pandas as pd
+from config import files_to_upload_dir, files_to_upload_dir, dataset_id, table_id
+from process_files import process_csv_files
+from upload_data_to_bigquery import get_bigquery_client, get_schema, load_csv_to_bigquery
 
-# # TODO: add logging
-# # TODO: this will be the base for uploading to big query
+# TODO: add logging
+# TODO: this will be the base for uploading to big query
 # TODO: add logging
 
 
-# def main():
-#     process_excel_files(files_to_upload_dir)
-#     # Initialize BigQuery client
-#     client = get_bigquery_client()
+def main():
+    # process_excel_files(files_to_upload_dir)
+    # Initialize BigQuery client
+    client = get_bigquery_client()
     
-#     # Get the schema
-#     schema = get_schema()
+    # Get the schema
+    schema = get_schema()
     
-#     load_csv_to_bigquery(client, schema, csvs_to_upload_dir, dataset_id, table_id)
+    load_csv_to_bigquery(client, schema, files_to_upload_dir, dataset_id, table_id)
     
-#     print("All files have been uploaded successfully.")
+    print("All files have been uploaded successfully.")
 
-# main()
+main()
--- main/process_files.py
+++ main/process_files.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from config import downloaded_csvs, files_to_upload_dir
 
-def process_excel_files(downloaded_raw_dir, upload_dir):
+def process_csv_files(downloaded_raw_dir, upload_dir):
     for filename in os.listdir(downloaded_raw_dir):
         filepath = os.path.join(downloaded_raw_dir, filename)
         if os.path.isfile(filepath) and filename.endswith('.csv'):
@@ -12,6 +12,15 @@ def process_excel_files(downloaded_raw_dir, upload_dir):
 
                 # Clean the headers
                 df = clean_headers(df)
+
+                # Ensure date column is in valid format
+                df = validate_date_format(df, 'date')
+
+                # Add a unique ID column
+                df = add_unique_id(df, ['id', 'name', 'date'])
+
+                # # Check for existing IDs before upload
+                # df = check_existing_ids(df, upload_dir, 'unique_id')
 
                 # Save cleaned DataFrame to a new CSV file
                 csv_filename = os.path.splitext(filename)[0] + '_processed.csv'
@@ -31,7 +40,35 @@ def clean_headers(df):
     )
     return df
 
+def validate_date_format(df, date_column):
+    if date_column in df.columns:
+        df[date_column] = pd.to_datetime(df[date_column], errors='coerce').dt.strftime('%Y-%m-%d')
+    return df
 
-    # TODO add a check on date to make sure it is bq format aka YYYY-MM-DD
-    # TODO add a unique id column
-    # TODO add a check before uploadin if id does not exit then upload
+def add_unique_id(df, columns):
+    if all(col in df.columns for col in columns):
+        df['unique_id'] = df[columns].astype(str).agg('_'.join, axis=1)
+    return df
+
+
+# TODO: refactor to check if it exists in db
+# def check_existing_ids(df, upload_dir, id_column):
+#     existing_ids = set()
+#     for filename in os.listdir(upload_dir):
+#         if filename.endswith('.csv'):
+#             existing_df = pd.read_csv(os.path.join(upload_dir, filename))
+#             if id_column in existing_df.columns:
+#                 existing_ids.update(existing_df[id_column].dropna().unique())
+    
+#     # Filter out rows with existing IDs
+#     if id_column in df.columns:
+#         df = df[~df[id_column].isin(existing_ids)]
+#     return df
+
+
+# Main function to process the CSV files
+def main():
+    process_csv_files(downloaded_csvs, files_to_upload_dir)
+
+if __name__ == "__main__":
+    main()
