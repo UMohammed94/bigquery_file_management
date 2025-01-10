9a97e805af6800cf236b224e752390ccd00db2e4
Mohammed Uddin <uddin.mohammed94@gmail.com>
2025-01-10 11:29:58 -0600

    cleaned up upload to bigquery

--- main/upload_data_to_bigquery.py
+++ main/upload_data_to_bigquery.py
@@ -3,58 +3,6 @@ from log import logger
 from google.cloud import bigquery
 
 
-# def get_bigquery_client():
-#     """Initialize and return a BigQuery client."""
-#     return bigquery.Client()
-
-# # TODO: can we do this dynamically?
-# def get_schema():
-#     """Return the schema for the BigQuery table."""
-#     return [
-#         bigquery.SchemaField('date', 'DATE'),
-#         bigquery.SchemaField('name', 'STRING'),
-#         bigquery.SchemaField('id','STRING'),
-#         bigquery.SchemaField('diameter_min_m','FLOAT64'),
-#         bigquery.SchemaField('diameter_max_m','FLOAT64'),
-#         bigquery.SchemaField('potentially_hazardous','BOOL'),
-#         bigquery.SchemaField('unique_id','STRING')
-# ]
-
-# def load_csv_to_bigquery(client, schema, csvs_to_upload_dir, project_id, dataset_id, table_id):
-#     """Load CSV files from a directory into a BigQuery table."""
-#     for filename in os.listdir(csvs_to_upload_dir):
-#         if filename.endswith('.csv'):
-#             file_path = os.path.join(csvs_to_upload_dir, filename)
-            
-#             # Define the BigQuery table reference
-#             table_ref = f"{project_id}.{dataset_id}.{table_id}"
-            
-#             # Configure the load job
-#             job_config = bigquery.LoadJobConfig(
-#                 schema=schema,
-#                 skip_leading_rows=1,  # Skip header row
-#                 source_format=bigquery.SourceFormat.CSV,
-#                 autodetect=False,  # Do not auto-detect schema
-#             )
-            
-#             # Load data from CSV file to BigQuery
-#             with open(file_path, "rb") as source_file:
-#                 load_job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
-            
-#             # Wait for the job to complete
-#             load_job.result()
-            
-#             logger.info(f"Loaded {filename} into {dataset_id}:{table_id}")
-
-# # ---------------
-
-# # verion 2
-
-import os
-from log import logger
-from google.cloud import bigquery
-
-
 def get_bigquery_client():
     """Initialize and return a BigQuery client."""
     return bigquery.Client()
